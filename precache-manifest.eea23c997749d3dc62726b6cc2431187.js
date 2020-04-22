abc5cb81402ba7c34dcf479a5465998b599f1068
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-22 17:57:19 +0000

    Deploy filip4428/deedit-frontend to github.com/filip4428/deedit-frontend.git:gh-pages

---
+++ precache-manifest.eea23c997749d3dc62726b6cc2431187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf1564d070c07f0221713d6741f50bd",
+    "url": "/deedit-frontend/index.html"
+  },
+  {
+    "revision": "0827891124d8e940cc74",
+    "url": "/deedit-frontend/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "6cfc523cce2e50c8560c",
+    "url": "/deedit-frontend/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "0827891124d8e940cc74",
+    "url": "/deedit-frontend/static/js/2.1d8b797c.chunk.js"
+  },
+  {
+    "revision": "6cfc523cce2e50c8560c",
+    "url": "/deedit-frontend/static/js/main.6821281a.chunk.js"
+  },
+  {
+    "revision": "7912bbcf8fbcc7e9ce2d",
+    "url": "/deedit-frontend/static/js/runtime~main.cb202c41.js"
+  }
+]);
